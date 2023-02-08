8234f52525cf428866e94c5adda360792f91d982
vhristova912 <viki66hristova@gmail.com>
2023-02-08 09:25:07 +0200

    Adding new details

---
+++ Arrays_1/Arrays_1/OptionsManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_1
+{
+    public static class OptionsManager
+    {
+        public static int CountLess100(int[] arr)
+        {
+            return arr.Count(x => x < 100);
+        }
+        public static int MaxNum(int[] arr)
+        {
+            return arr.Max();
+        }
+        public static int[] ReverseArr(int[] arr)
+        {
+            return arr.Reverse().ToArray();
+        }
+        public static void EvenNums(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+        public static int SumEvenNums(int[] arr)
+        {
+            return arr.Where(x => x % 2 == 0).Sum();
+
+        }
+        public static int CountEvenNums(int[]arr)
+        {
+            return arr.Where(x => x % 2 == 0).Count();
+        }
+        public static void PrintArr(int[]arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
---
+++ Arrays_1/Arrays_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Count= " + OptionsManager.CountLess100(arr));
+            Console.WriteLine("Max= " + OptionsManager.MaxNum(arr));
+            OptionsManager.EvenNums(arr);
+
+
+            arr = OptionsManager.ReverseArr(arr);
+            Console.WriteLine("sumEvens= " + OptionsManager.SumEvenNums(arr));
+            Console.WriteLine("countEvens= " + OptionsManager.CountEvenNums(arr));
+            OptionsManager.PrintArr(arr);  
+        }
+    }
+}
